eb720869550ba6de000106afea75bdf11b44e436
Udita Singh <udita1294@gmail.com>
2026-08-09 00:37:59 +0530

    Initial setup of the project done

---
+++ rag_intro.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+client = Groq(api_key=my_api_key)
+model = "llama-3.3-70b-versatile"
+
